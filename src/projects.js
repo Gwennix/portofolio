e3aa01df1b0536faa8f946a3ab1707d43b612eae
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-06-10 14:38:08 +0200

    Update

--- src/projects.js
+++
@@ -1,9 +0,0 @@
-
-const projects = [
-    {id: 1, name: 'Promotie Site', description: 'Een promotie site concept voor een game. Gebruikt HTML & CSS.', imageSrc: '/xy.png', link: 'https://github.com/Gwennix/my-sites/tree/main/pokemon-xy', code: '/src/img/html.png', code2: '/src/img/css.png', code3:'/src/img/placeholder.png'},
-    {id: 2, name: 'Runner Game', description: 'Een Javascript game.', imageSrc: '/game.png', link:'https://github.com/Gwennix/my-sites/tree/main/pokemon-game', code: '/src/img/html.png', code2: '/src/img/css.png', code3:'/src/img/javascript.png'},
-    {id: 3, name: 'Webshop Concept', description: 'Concept voor een webshop voor een digitale boekenwinkel.', link:'https://github.com/Gwennix/my-sites/tree/main/webshop-concept', imageSrc: '/webshop.png', code: '/src/img/html.png', code2: '/src/img/css.png', code3:'/src/img/javascript.png'},
-    {id: 4, name: 'Web Education Project', description: 'Collab voor een site die les geeft in programmeren.', link:'https://github.com/Gwennix/my-sites/tree/main/web-education-site', imageSrc: '/education.png', code: '/src/img/html.png', code2: '/src/img/css.png', code3:'/src/img/javascript.png'},
-];
-
-export default projects;
